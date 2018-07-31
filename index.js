695582a2f05368545ddbae6a5b1011abf3365080
ulisses.montenegro <ulisses.montenegro@gmail.com>
2018-07-31 01:12:00 -0400

    Add support for JWT signining/generation

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const config = require('config')
 const express = require('express')
 const expressWinston = require('express-winston')
+const jsonwebtoken = require('jsonwebtoken')
 const jwt = require('express-jwt')
 const winston = require('winston')
 
@@ -11,9 +13,13 @@ app.locals.logger = winston.createLogger({
 })
 app.use(expressWinston.logger({ winstonInstance: app.locals.logger }))
 
+/* Enable body parsers */
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
 /* Enable JWT authentication */
 app.use(jwt({
-  secret: 'thisIsMySuperAwesomeSecret',
+  secret: config.jwt.sharedSecret,
   credentialsRequired: true,
   getToken: (req) => {
     if (req.headers.authorization && req.headers.authorization.split(/\s+/)[0] === 'Bearer') {
@@ -21,22 +27,35 @@ app.use(jwt({
       const header = JSON.parse(Buffer.from(token.split(/\./)[0], 'base64').toString('utf8'))
       
       /* Only return the secret after we validated the header */
-      if (header && header.alg === 'HS512') {
+      if (header && header.alg === config.jwt.algorithm) {
         return token
       }
       app.locals.logger.warn({ error: 'Invalid header parameters', header })
     }
     return null
   }
+}).unless({
+  path: [ '/login' ]
 }))
 
-/* Enable custom error handling */
+/* Enable custom error handling; catpure any JWT authentication errors */
 app.use((err, req, res, next) => {
   if (err.name === 'UnauthorizedError') {
     return res.status(401).json({ status: 'Unauthorized' })
   }
 })
 
+/* Anything that is posted to /login will get signed and turned into a valid JWT */
+app.post('/login', (req, res) => {
+  jsonwebtoken.sign(req.body, config.jwt.sharedSecret, { algorithm: config.jwt.algorithm }, (err, token) => {
+    if (err) {
+      return res.send(500).json({ error: err })
+    }
+    return res.status(200).json({ payload: req.body, jwt: token })
+  })
+})
+
+/* Any other URL will just output the JWT claims back as a JSON document */
 app.all(/\S*/, (req, res) => {
   return res.status(200).json({ user: req.user })
 })
